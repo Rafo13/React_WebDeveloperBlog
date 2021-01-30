d233f51ad45db2007f3479fb5d37ae1837c58885
Rafo13 <rafoarakelyan33gmail.com>
2021-01-31 00:28:54 +0400

    avartecinq 3-rd dasy, menu-n, ev router-y

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+   render() {
+      return (
+         <div>
+            Blog
+         </div>
+      );
+   }
+}
+
+export default Home;
